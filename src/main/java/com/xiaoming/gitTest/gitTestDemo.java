36211ca83d97ce3ecd4ecd824e0025b420bd7795
Liam <Liam@love.com>
2022-07-19 03:11:24 +0800

    hot-fix second commit

--- src/main/java/com/xiaoming/gitTest/gitTestDemo.java
+++ src/main/java/com/xiaoming/gitTest/gitTestDemo.java
@@ -13,5 +13,6 @@ public class gitTestDemo {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
