51c892adaeea1f82c89ea188d5c8fe870f3d9fa2
Liam <Liam@love.com>
2022-07-19 03:04:02 +0800

    first commit

---
+++ src/main/java/com/xiaoming/gitTest/gitTestDemo.java
@@ -0,0 +1,14 @@
+package com.xiaoming.gitTest;
+
+public class gitTestDemo {
+    /**
+     * @title: gitTestDemo
+     * @Author DRose
+     * @Date: 2022/7/19 3:03
+     * @PACKAGE_NAME: com.xiaoming.gitTest
+     */
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
